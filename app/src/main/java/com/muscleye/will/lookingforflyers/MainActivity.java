acbb0abb0fa83f612eb3cf3621ba98622b5c6bef
Willy-Wonka <willwcc@gmail.com>
2015-03-30 08:16:42 -0700

    RESTful lesson 5 ing

--- app/src/main/java/com/muscleye/will/lookingforflyers/MainActivity.java
+++ app/src/main/java/com/muscleye/will/lookingforflyers/MainActivity.java
@@ -1,19 +1,77 @@
 package com.muscleye.will.lookingforflyers;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+    String str1;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the TextView for vertical scrolling
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+
+        //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
+        //startActivity(browserIntent);
+        Log.d("url", "start getPage");
+
+        for (int i = 0; i < 100; i++)
+        {
+            updateDisplay("line " + i);
+        }
+
     }
 
+    public static String getPage(String uri)
+    {
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(uri);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (reader != null)
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,13 +85,17 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == R.id.action_settings)
+        {
+            updateDisplay("Task Done!");
         }
+        return false;
+    }
 
-        return super.onOptionsItemSelected(item);
+    protected void updateDisplay(String message)
+    {
+        output.append(message +"\n");
     }
 }
